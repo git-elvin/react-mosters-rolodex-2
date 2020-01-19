9c914dfc048d61f63ca0995e285d437e6ce085dc
git-elvin <elvinalvarez@outlook.es>
2020-01-19 09:49:03 -0600

    Updates

---
+++ precache-manifest.3eee5c5c9da7205c68fa242d76cc36cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a44f4bd001e5b007dc20caa732b8dd34",
+    "url": "/react-mosters-rolodex-2/index.html"
+  },
+  {
+    "revision": "caf4c9020eb3d5606409",
+    "url": "/react-mosters-rolodex-2/static/css/main.a3311169.chunk.css"
+  },
+  {
+    "revision": "0193fd501bbadc1438a6",
+    "url": "/react-mosters-rolodex-2/static/js/2.c9114c39.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-mosters-rolodex-2/static/js/2.c9114c39.chunk.js.LICENSE"
+  },
+  {
+    "revision": "caf4c9020eb3d5606409",
+    "url": "/react-mosters-rolodex-2/static/js/main.629e0ed0.chunk.js"
+  },
+  {
+    "revision": "88cdf564547da0890f41",
+    "url": "/react-mosters-rolodex-2/static/js/runtime-main.5f184266.js"
+  }
+]);
